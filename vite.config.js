2332052b833507b23180fbf2f104a9e1f72df8bc
ocholi divine <ocholidivine41@gmail.com>
2025-11-21 21:29:14 +0100

    addressed the issue of link no directing you to the right page

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [react()],
-  root: 'Verapixels',
-  base: '/verapixels/',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-    assetsDir: 'assets'
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './Verapixels/src')
+export default defineConfig(({ command }) => {
+  const base = command === 'serve' ? '/' : '/verapixels/'
+  
+  return {
+    plugins: [react()],
+    root: 'Verapixels',
+    base: base,
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+      assetsDir: 'assets'
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './Verapixels/src')
+      }
     }
   }
 })
