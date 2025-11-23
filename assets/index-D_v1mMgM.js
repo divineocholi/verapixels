3ee453fef86ad83d465a39a729a7bb3b8f29e969
ocholi divine <ocholidivine41@gmail.com>
2025-11-23 17:52:42 +0100

    Updates

--- assets/index-D_v1mMgM.js
+++ assets/index-D_v1mMgM.js
@@ -3414,13 +3414,14 @@ Please change the parent <Route path="${z}"> to <Route path="${z==="/"?"*":`${z}
         }
 
         .stats-grid {
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  display: flex;
+  flex-wrap: wrap;
   gap: 30px;
-  justify-items: center;
+  justify-content: center;
+  align-items: center;
 }
 
- .stat-card {
+.stat-card {
   position: relative;
   padding: 40px 30px;
   background: rgba(255, 255, 255, 0.02);
@@ -3430,13 +3431,13 @@ Please change the parent <Route path="${z}"> to <Route path="${z==="/"?"*":`${z}
   transition: all 0.4s ease;
   overflow: hidden;
   animation: fadeInUp 0.8s ease both;
-  width: 100%;
-  max-width: 300px;
+  width: 280px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
- }
+}
+
 
         .stat-card:hover {
           transform: translateY(-10px);
